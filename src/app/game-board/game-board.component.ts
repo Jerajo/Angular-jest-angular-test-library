066a78aec6306c9ec0de399d136f1d65deb67d82
jesse.jose <JJoseDelaRosa.Simtlix@supervielle.com.ar>
2024-06-13 14:07:30 -0300

    probar hook global

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -19,10 +19,10 @@ export class GameBoardComponent implements OnInit, AfterViewInit {
     const score = this.food.currentScore;
     if (score < 5) {
       return 10;
-    } else if (score >= 5 && score < 10) {
+    } else if (score >= 5 && score < 10) {   
       return 13;
-    } else if (score >= 10 && score < 20) {
-      return 16;
+    } else if (score >= 10 && score < 20) {   
+      return 16;   
     }
     return 20;
   }
