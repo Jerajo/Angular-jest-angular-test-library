eff058e630c8e6fcba11350cada269a8e37e0ff2
jesse.jose <JJoseDelaRosa.Simtlix@supervielle.com.ar>
2024-06-13 10:15:06 -0300

    tslint configurado

--- src/app/game-engine/gameboard-grid.util.ts
+++ src/app/game-engine/gameboard-grid.util.ts
@@ -1,20 +1,20 @@
 const GRID_SIZE = 21;
 
 export interface GridPosition {
-  x: number;
+  x: number;  
   y: number;
-}
+}  
 
-export function randomGridPosition(): GridPosition {
+export function randomGridPosition(): GridPosition {  
   return {
-    x: Math.floor(Math.random() * GRID_SIZE) + 1,
-    y: Math.floor(Math.random() * GRID_SIZE) + 1,
+    x: Math.floor(Math.random() *    GRID_SIZE) + 1,
+    y: Math.floor(Math.random() * GRID_SIZE) + 1,  
   };
 }
 
 export function outsideGrid(position: any): boolean {
   return (
-    position.x < 1 ||
+    position.x < 1 ||  
     position.x > GRID_SIZE ||
     position.y < 1 ||
     position.y > GRID_SIZE
